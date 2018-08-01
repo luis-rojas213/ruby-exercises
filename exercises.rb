45f7d7ffcadd49f8e5c278947d81a2f045db1b25
luis-rojas213 <luis.rojas213@gmail.com>
2018-08-01 10:32:01 -0400

    Revert "se agrega comentarios YARD y pruebas RSPEC, se corrige solución de ejercicios"
    This reverts commit b6c3076ccabcc1787ff0fed0e3870ce8ad148e1a.

--- exercises.rb
+++ exercises.rb
@@ -1,23 +1,18 @@
 class Exercises
-  # Find a longer word.
-  #
-  # @param [Array] words the compare length
-  # @return [String] Longest word found.
   def long_word(*words)
-    words.map(&:to_s).max_by(&:size)
+    max_length = ''
+
+    words.each do |word|
+      max_length = word if word.size > max_length.size
+    end
+
+    max_length
   end
 
-  # Random number between 10.00 and 100.00
-  #
-  # @return [String] Generated number.
   def random_number
     rand(10.00..100.00).round(2)
   end
 
-  # Convert uppercase to lowercase and lowercase to uppercase
-  #
-  # @param [String] word to convert
-  # @return [String] Converted word.
   def swapcase(word)
     raise ArgumentError unless word.is_a?(String)
     word.to_s.chars.map { |w| w.downcase! ? w : w.upcase }.join
