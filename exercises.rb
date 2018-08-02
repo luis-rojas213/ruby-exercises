f9230005caf53ef2492d3057067df0a1433cc12f
luis-rojas213 <luis.rojas213@gmail.com>
2018-08-02 00:36:19 -0400

    se agregan archivos de pruebas y correcciones en los ejercicios

--- exercises.rb
+++ exercises.rb
@@ -1,18 +1,24 @@
-class Exercises
+module Exercises
+  extend self
+  # Find a longer word.
+  #
+  # @param [Array] words the compare length
+  # @return [String] Longest word found.
   def long_word(*words)
-    max_length = ''
-
-    words.each do |word|
-      max_length = word if word.size > max_length.size
-    end
-
-    max_length
+    words.map(&:to_s).max_by(&:size)
   end
 
+  # Random number between 10.00 and 100.00
+  #
+  # @return [String] Generated number.
   def random_number
     rand(10.00..100.00).round(2)
   end
 
+  # Convert uppercase to lowercase and lowercase to uppercase
+  #
+  # @param [String] word to convert
+  # @return [String] Converted word.
   def swapcase(word)
     raise ArgumentError unless word.is_a?(String)
     word.to_s.chars.map { |w| w.downcase! ? w : w.upcase }.join
